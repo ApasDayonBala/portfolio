55910652c6d763f2977e2c1eaa32defd34d31291
kevinlloyd Escanuela <ApasDayonBala>
2024-10-29 14:46:13 +0300

    modefied first commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -24,7 +24,7 @@ export const Contact = () => {
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/kevin</a>
+          <a href="https://www.github.com/myname">github.com/apasdayonbala</a>
         </li>
       </ul>
     </footer>
